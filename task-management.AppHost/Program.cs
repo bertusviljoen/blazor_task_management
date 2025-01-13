0c6e58cbea20cb1c3da05459dc38827d012edeeb
Bertus Viljoen <Bertus Viljoen>
2025-01-13 06:58:06 +0200

    refactor(pages): clean up unused code and improve readability
    Removed commented-out code and unnecessary using directives in
    `TaskEditDialog.razor` and `TaskBoardEditDialog.razor`. Reformatted
    lines in `TaskBoard.razor` for better readability and maintainability.

--- task-management.AppHost/Program.cs
+++ task-management.AppHost/Program.cs
@@ -16,9 +16,9 @@ var apiService = builder.AddProject<Projects.task_management_ApiService>("apiser
         .WithReference(cosmos)
         .WaitFor(cosmos);
 
-// builder.AddProject<Projects.task_management_Web>("webfrontend")
-//     .WithExternalHttpEndpoints()
-//     .WithReference(apiService)
-//     .WaitFor(apiService);
+builder.AddProject<Projects.task_management_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService)
+    .WaitFor(apiService);
 
 builder.Build().Run();
